dfef84220854a05443c4b4dce1ac75acee9de06d
Adam Driscoll <adam.r.driscoll@gmail.com>
2020-10-22 10:56:07 -0600

    Fix work around.

--- Repository/DisableRoles.ps1
+++ Repository/DisableRoles.ps1
@@ -1,3 +1,3 @@
-Invoke-RestMethod -Method Post -Uri http://host.docker.internal:5000/api/v1/signin -Body (@{ userName = 'admin'; password = 'any'} | ConvertTo-Json) -SessionVariable session -ContentType 'application/json'
-Invoke-RestMethod -Uri http://host.docker.internal:5000/api/v1/role/1 -Method DELETE -WebSession $session 
-Invoke-RestMethod -Uri http://host.docker.internal:5000/api/v1/role/2 -Method DELETE -WebSession $session   
+Invoke-RestMethod -Method Post -Uri http://127.0.0.1:5000/api/v1/signin -Body (@{ userName = 'admin'; password = 'any'} | ConvertTo-Json) -SessionVariable session -ContentType 'application/json'
+Invoke-RestMethod -Uri http://127.0.0.1:5000/api/v1/role/1 -Method DELETE -WebSession $session 
+Invoke-RestMethod -Uri http://127.0.0.1:5000/api/v1/role/2 -Method DELETE -WebSession $session   
